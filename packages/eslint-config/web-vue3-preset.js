a29c29f2faa1dc29a86e537dd7776919551ec745
Reidond <reidond@gmail.com>
2021-12-21 18:39:02 +0200

    fix: eslint vue config

--- packages/eslint-config/web-vue3-preset.js
+++ packages/eslint-config/web-vue3-preset.js
@@ -1,6 +1,20 @@
 module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        "vue/setup-compiler-macros": true,
+    },
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+        ecmaVersion: "latest",
+    },
+    plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
         "plugin:vue/vue3-recommended",
         "plugin:prettier/recommended",
     ],
